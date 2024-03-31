e4d13cff5568042a8ec8e15b45a852d2d08dcc5c
Egor <95527517+EgoR202040@users.noreply.github.com>
2024-03-31 15:54:57 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,33 +5,22 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
+tg.MainButton.setText("Отправить");
+tg.MainButton.show();
 
-let submit = document.getElementById("submit");
 
-submit.addEventListener("click", function(){
-	document.getElementById('error').innerText = '';
-	let name = document.getElementById('user_name').value;
-	let mail = document.getElementById('user_mail').value;
-	let password = document.getElementById('user_pass').value;
-
-	if(name.length() < 5){
-		document.getElementById('error').innerText = 'Ошибка в имени';
-	}
-	if(mail.length() < 5){
-		document.getElementById('error').innerText = 'Ошибка в почте';
-	}
-	if(password.length() < 5){
-		document.getElementById('error').innerText = 'Ошибка в пароле';
-	}
-	let data = {
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	let name = document.getElementById("user_name");
+	let email = document.getElementById("user_mail");
+	let password = document.getElementById("user_pass");
+	data = {
 		name: name,
-		mail: mail,
-		password: password
+		email:email,
+		password:password
 	}
-	tg.sendData(JSON.stringify(data))
+	tg.sendData(JSON.stringify(data));
 	tg.close()
 });
-
 
 
 
